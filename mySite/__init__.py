df18c1c26234b3c90c8e03f388a078f29987c49d
AlexanderMcNulty <alexander.mcnulty92@gmail.com>
2017-09-14 03:29:54 -0700

    serve angular client as static asset

--- mySite/__init__.py
+++ mySite/__init__.py
@@ -3,9 +3,11 @@ from pyramid.config import Configurator
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
-    config.include('pyramid_jinja2')
-    # this is where the js lives
-    config.add_static_view(name='static', path='mysite:static')
-    config.add_route('home', '/')
+    config.add_route('greeting', '/greeting')
     config.scan()
+
+    # Statically serve app directory at the "/" URL
+    config.add_route('app_html', '/')
+    config.add_static_view(name='/', path='mysite:app')
+
     return config.make_wsgi_app()
--- mysite/views.py
+++ mysite/views.py
@@ -1,31 +1,20 @@
+import os
+
+from pyramid.response import FileResponse
 from pyramid.view import view_config
 
 
 class MySite:
     def __init__(self, request):
         self.request = request
+        here = os.path.dirname(__file__)
+        self.index_path = os.path.join(here, 'app', 'index.html')
 
-    @view_config(route_name='home', renderer='templates/home.jinja2')
-    def home(self):
-        return dict()
+    @view_config(route_name='app_html')
+    def index_view(self):
+        return FileResponse(self.index_path, request=self.request)
 
-    @view_config(route_name='home', renderer='json', request_method='POST')
-    def greeting(self):
-        # request.params vs request.json_body
-        # to learn more read "Dealing with a JSON-encoded request body"
-        # browser now sends and returns json
-        greeting = 'Hello %s!' % self.request.json_body.get('name')
-        return dict(greeting=greeting)
-
-
-    '''
-    # renderer user to pass templates, now we can use it to pass json
-    # passing data will change the response headers
     @view_config(route_name='greeting', renderer='json')
     def greeting(self):
-        # request.params vs request.json_body
-        # to learn more read "Dealing with a JSON-encoded request body"
-        # browser now sends and returns json
         greeting = 'Hello %s!' % self.request.json_body.get('name')
         return dict(greeting=greeting)
-    '''
