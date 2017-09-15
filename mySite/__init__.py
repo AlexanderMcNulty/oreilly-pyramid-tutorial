deb385e0a1a06b36e7ff0f45c62abe949e07a2e4
AlexanderMcNulty <alexander.mcnulty92@gmail.com>
2017-09-14 22:45:58 -0700

    REST, with sqlite - simple example

--- mySite/__init__.py
+++ mySite/__init__.py
@@ -1,13 +1,18 @@
 from pyramid.config import Configurator
+from pyramid_sqlalchemy import metadata
 
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
-    config.add_route('greeting', '/greeting')
+    config.include('pyramid_sqlalchemy')
+    config.add_route('todos', '/api/todos')
+    config.add_route('todo', '/api/todos/{id}',
+                     factory='.models.todo_factory')
     config.scan()
+    metadata.create_all()
 
     # Statically serve app directory at the "/" URL
-    config.add_route('app_html', '/')
     config.add_static_view(name='/', path='mysite:app')
+    config.add_route('app_html', '/')
 
     return config.make_wsgi_app()
--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup
 
 requires = [
     'pyramid',
-    'pyramid_jinja2'
+    'pyramid_sqlalchemy',
+    'pyramid_tm'
+
 ]
 setup(name='mysite',
       install_requires=requires,
       entry_points="""\
       [paste.app_factory]
       main = mysite:main
+      [console_scripts]
+      initialize_db = mysite.scripts.initialize_db:main
       """
       )
